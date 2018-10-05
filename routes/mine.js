cc2001e196c6d97e7567c6ae152bf9be988dda8b
jcuevas012 <jcq012@gmail.com>
2018-10-04 23:13:00 -0400

    all enpoints added

--- routes/mine.js
+++ routes/mine.js
@@ -1,34 +1,33 @@
 const express = require('express')
-const app =  express.Router()
+const app = express.Router()
 const db = require('../db')
 
 db.init()
 const { Game } = require('../db/models')
 
-function generatePosition (row, col) {
+function generatePosition(row, col) {
     rowArr = []
     for (let i = 0; i < row; i++) {
-        let colArr =[]
+        let colArr = []
         for (let j = 0; j < col; j++) {
-            value = Math.floor(Math.random() * 2) + 1              
-            colArr.push(value)
+            colArr.push(1)
         }
-       rowArr.push(colArr)
+        rowArr.push(colArr)
     }
     return rowArr
 }
 
 
 function setMines(matrix, num) {
-    
+
     const row = matrix.length
     const col = matrix[0].length
-    
+
     for (let i = 0; i < num; i++) {
-        value = Math.floor(Math.random() * (col * row) + 1) + 1    
+        value = Math.floor(Math.random() * (col * row) + 1) + 1
         let x = parseInt(value.toString().charAt(0))
         let y = parseInt(value.toString().charAt(1))
-        matrix[x][y] = 0 
+        matrix[x][y] = 0
     }
 
     return matrix
@@ -42,35 +41,35 @@ function generateGame(level) {
 
     if (level == 'mid') {
         matrix.col = 20,
-        matrix.row = 20,
-        matrix.arr = generatePosition(matrix.row, matrix.col)
+            matrix.row = 20,
+            matrix.arr = generatePosition(matrix.row, matrix.col)
         matrix.arr = setMines(matrix.arr, 14)
         return matrix
 
     } else if (level == 'pro') {
         matrix.col = 30,
-        matrix.row = 30,
-        matrix.arr = generatePosition(matrix.row, matrix.col)
+            matrix.row = 30,
+            matrix.arr = generatePosition(matrix.row, matrix.col)
         matrix.arr = setMines(matrix.arr, 21)
         return matrix
     } else {
-         matrix.arr = generatePosition(matrix.row, matrix.col)
-         matrix.arr = setMines(matrix.arr, 7)
-         return matrix
+        matrix.arr = generatePosition(matrix.row, matrix.col)
+        matrix.arr = setMines(matrix.arr, 7)
+        return matrix
     }
-    
+
 }
 
 
-app.post('/start',  async (req, res) => {
+app.post('/start', async (req, res) => {
     const matrix = generateGame(req.query.level)
-    
+
     const game = await Game({
         play: matrix.arr,
         row: matrix.row,
         col: matrix.col,
         startTime: Date(),
-        userId: 'sdd',
+        userId: req.query.userId,
     }).save()
 
     const response = {
@@ -79,28 +78,87 @@ app.post('/start',  async (req, res) => {
         row: game.row,
         startTime: game.startTime,
     }
-
     res.send(response)
 })
 
 
 app.post('/open', async (req, res) => {
-    const params = req.body
-    const data = await Game.findById(params.id)
-    value = data.play[params.row][params.col]
+    const row = req.body.row + 1
+    const col = req.body.col + 1
+
+    if (!row || !col) res.status(500).send({ message: 'Should specify row and column where to play'})
     
+    const game = await Game.findById(req.body.id)
+    
+    if (game.isOver || game.pause) res.status(500).send({ message: 'The game is over or pause, you can not play'})
+    
+
+    value = game.play[row][col]
+
     if (!value) {
         game.isOver = true
         game.endTime = Date()
         const data = await game.save()
-        res.send({ value, message: 'Game Over ', game: data })  
-    } 
-    res.send({ value, message: 'Keep going !'})
+        res.send({ value, message: 'Game Over ', game: data })
+    }
+    value += 1
+    game.play[row][col] = value
+    await game.save()
 
+    res.send({ value, message: 'Keep going !' })
+})
+
+
+
+app.put('/pause/:id', async (req, res) => {
+    const id = req.params.id
+    const game = await Game.findById(id)
+
+    if (game.isOver)
+        res.status(500).send({ message: 'Game is over, it can not be pause' })
+
+    game.pause = !game.pause
+    const updated = await game.save()
+
+    res.send({ pause: updated.pause, id: updated._id })
+})
+
+
+
+app.put('/flag/:id', async (req, res) => {
+    const id = req.params.id
+    const  params = req.body
+    
+    if (!params.row || !params.col) res.status(500).send({ message: 'Should specify row and column where to play'})
+
+    const game = await Game.findById(id)
+    const play = [ ...game.play ]
+
+    if (game.isOver || game.pause) res.status(500).send({ message: 'The game is over or pause, you can not play'})
+
+    play[params.row - 1][params.col - 1] = '?'
+    const value = play[params.row - 1][params.col - 1]
+    game.play = play
+    const updated = await game.save()
+
+
+    res.send({ col: params.col, row: params.row, value })
 })
 
 
 
 
+app.get('/games/:userId', async (req, res) => {
+    const userId = req.params.userId
+    
+    let games = await Game.find({ userId })
+    games  = games.map(game => {
+        let newGame = JSON.parse(JSON.stringify(game))
+        delete newGame.play
+        return newGame
+    })
+
+    res.send(games)
+})
 
 module.exports = app
